e5a38dda03f44c497631d4a28f9fb18b560d9886
Adam Argyle <atom@argyleink.com>
2018-01-15 20:29:57 -0800

    fixes disappearing titlebar when scrolling is at bottom

--- app/js/components/chat-ui/chat-scrollview/chat-scrollview.element.js
+++ app/js/components/chat-ui/chat-scrollview/chat-scrollview.element.js
@@ -14,7 +14,7 @@ export default class ChatScrollview extends HTMLElement {
 
     // show/hide based on scroll direction
     this.onscroll = e => {
-      if (this.oldScroll > e.currentTarget.scrollTop && this.scrollTop != this.scrollHeight)
+      if (this.oldScroll > e.currentTarget.scrollTop && this.clientHeight + this.scrollTop != this.scrollHeight)
         document.querySelector('chat-titlebar').hide()
       else
         document.querySelector('chat-titlebar').show()
