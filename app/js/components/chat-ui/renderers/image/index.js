5db35b9d29207e6a47225d3e49950e1c4b37b59c
Adam Argyle <atom@argyleink.com>
2018-01-20 14:34:17 -0800

    continue noting and thinking about chat-media element

--- app/js/components/chat-ui/renderers/image/index.js
+++ app/js/components/chat-ui/renderers/image/index.js
@@ -2,11 +2,14 @@ import './image.css'
 
 // TODO: custom element
 // async image loading strategy
+// handle array of images
 export const Image = ({author, image}) => `
-  <figure class="chat-image">
-    <img src="${image}">
-    <figcaption>
-      <chat-avatar src="${author.avatar}"></chat-avatar>
-    </figcaption>
-  </figure>
+  <chat-media>
+    <figure>
+      <img src="${image}">
+      <figcaption>
+        <chat-avatar src="${author.avatar}"></chat-avatar>
+      </figcaption>
+    </figure>
+  </chat-media>
 `
