1d272869e84d24adfc6e3781180ed2f59976513b
Adam Argyle <atom@argyleink.com>
2018-01-20 14:33:33 -0800

    multiple cards horizontally scroll

--- app/js/components/chat-ui/renderers/card/index.js
+++ app/js/components/chat-ui/renderers/card/index.js
@@ -2,18 +2,21 @@ import './card.css'
 
 // TODO: custom element
 // async image loading strategy
+// handle array of cards
 export const Card = ({author, image, text}) => `
-  <div class="chat-card" grid="rows" vertically-distributed="equal">
-    <div class="card-header" grid vertically-aligned="center">
-      <chat-avatar src="${author.avatar}"></chat-avatar>
-    </div>
-    <div class="card-content">
-      <img src="${image}"/>
-      <p>${text}</p>
-    </div>
-    <div class="card-footer" grid vertically-aligned="center">
-      <a href="href">Like</a>
-      <a href="href">Comment</a>
-    </div>
-  </div>
+  <chat-cards>
+    <chat-card grid="rows" vertically-distributed="equal">
+      <div class="card-header" grid vertically-aligned="center">
+        <chat-avatar src="${author.avatar}"></chat-avatar>
+      </div>
+      <div class="card-content">
+        <img src="${image}"/>
+        <p>${text}</p>
+      </div>
+      <div class="card-footer" grid vertically-aligned="center">
+        <a href="href">Like</a>
+        <a href="href">Comment</a>
+      </div>
+    </chat-card>
+  </chat-cards>
 `
