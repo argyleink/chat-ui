e468509e3f22da9165b5c3a55ff0b8c1473b6e0f
Adam Argyle <atom@argyleink.com>
2018-01-20 23:16:57 -0800

    fix spacing, add not of renderer to make

--- app/js/components/chat-ui/renderers/index.js
+++ app/js/components/chat-ui/renderers/index.js
@@ -1,12 +1,13 @@
-import {HTML}   from './html'
-import {Text}   from './text'
-import {Image, ChatGallery}  from './image/'
-import {Card, ChatCards}   from './card/'
+import {HTML} from './html'
+import {Text} from './text'
+import {Image, ChatGallery} from './image/'
+import {Card, ChatCards} from './card/'
 
 export { 
   HTML, 
   Text, 
   Image,
+  // image gallery
   Card,
   // links
 }
