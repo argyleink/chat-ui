cdad97319ad0e39365b79eaebbb53cf1a0abd743
Adam Argyle <atom@argyleink.com>
2018-01-24 19:36:14 -0800

    remove text renderer, obselete

--- app/js/components/chat-ui/renderers/index.js
+++ app/js/components/chat-ui/renderers/index.js
@@ -1,5 +1,4 @@
 export {HTML} from './html'
-export {Text} from './text'
 export {ChatMedia, ImageTemplate as Image} from './media'
 export {ChatGallery, GalleryTemplate as Gallery} from './gallery/'
 export {ChatCards, CardsTemplate as Card} from './card/'
